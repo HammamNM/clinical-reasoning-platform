10ff82bc609a0c971e62c218189c50e6b8d8170b
HammamNM <hammamomer304@gmail.com>
2026-07-20 22:51:23 +0200

    Update graph.py

--- kernel/graph.py
+++ kernel/graph.py
@@ -1,1 +1,16 @@
+class ReasoningGraph:
 
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def add_edge(self, source, target):
+        self.edges.append(
+            {
+                "source": source,
+                "target": target
+            }
+        )
