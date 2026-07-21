0ddea26fbc6993096332a5f3372749b7908e4326
HammamNM <hammamomer304@gmail.com>
2026-07-21 12:13:52 +0200

    Update rules.py

--- simulation/rules.py
+++ simulation/rules.py
@@ -1,1 +1,31 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class SimulationRule:
+
+    trigger_action: str
+    effect: str
+
+
+class RuleEngine:
+
+    def __init__(self):
+        self.rules = []
+
+    def add_rule(self, rule):
+        self.rules.append(rule)
+
+    def process_action(self, action, state):
+
+        for rule in self.rules:
+
+            if rule.trigger_action == action:
+
+                if rule.effect == "REVEAL_ECG":
+                    state.reveal_ecg()
+
+                elif rule.effect == "REVEAL_TROPONIN":
+                    state.reveal_troponin()
+
+                state.add_action(action)
