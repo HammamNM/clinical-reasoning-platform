664f06859161268402015d6c03c4da9bd5d032e8
HammamNM <hammamomer304@gmail.com>
2026-07-21 17:44:47 +0200

    Create orchestrator.py

---
+++ simulation/orchestrator.py
@@ -0,0 +1,45 @@
+from simulation.timeline import TimelineEngine
+from simulation.rules import RuleEngine
+
+
+class SimulationOrchestrator:
+
+
+    def __init__(
+        self,
+        session
+    ):
+
+        self.session = session
+
+        self.rule_engine = RuleEngine()
+
+        self.timeline_engine = TimelineEngine()
+
+
+
+    def process_student_action(
+        self,
+        action
+    ):
+
+        self.rule_engine.process_action(
+            action,
+            self.session.state
+        )
+
+
+        self.session.clock.advance(1)
+
+
+        timeline_events = (
+            self.timeline_engine
+            .get_events_at_time(
+                self.session.clock.get_time()
+            )
+        )
+
+
+        for event in timeline_events:
+
+            self.session.event_stream.add(event)
