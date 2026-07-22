a79e841f0550cd456787645af68a15a0f0352053
HammamNM <hammamomer304@gmail.com>
2026-07-22 08:47:42 +0200

    Update engine.py

--- decision_engine/engine.py
+++ decision_engine/engine.py
@@ -1,14 +1,24 @@
 from decision_engine.models import DecisionProfile
 from decision_engine.evaluator_manager import EvaluatorManager
-
+from decision_engine.evaluators.timing_evaluator import TimingEvaluator
+from decision_engine.evaluators.safety_evaluator import SafetyEvaluator
 
 class DecisionEngine:
 
 
-    def __init__(self):
+   def __init__(self):
 
-        self.evaluator_manager = EvaluatorManager()
+    self.evaluator_manager = EvaluatorManager()
 
+
+    self.register_evaluator(
+        TimingEvaluator()
+    )
+
+
+    self.register_evaluator(
+        SafetyEvaluator()
+    ) 
 
 
     def register_evaluator(
