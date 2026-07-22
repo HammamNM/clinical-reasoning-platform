469a6b19b9122bff5362e8b3c266a104f74058c7
HammamNM <hammamomer304@gmail.com>
2026-07-22 08:18:55 +0200

    Create test_integration.py

---
+++ tests/test_integration.py
@@ -0,0 +1,21 @@
+from simulation.session import ClinicalSession
+from simulation.orchestrator import SimulationOrchestrator
+
+
+session = ClinicalSession()
+
+orchestrator = SimulationOrchestrator(
+    session
+)
+
+
+orchestrator.process_student_action(
+    "ORDER_ECG"
+)
+
+
+print("EVENTS:")
+
+for event in session.event_stream.events:
+
+    print(event)
