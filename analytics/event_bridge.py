4605dc0a3c7c210bf695d86ed7fca9004e3ad319
HammamNM <hammamomer304@gmail.com>
2026-07-23 12:42:41 +0200

    Update event_bridge.py

--- analytics/event_bridge.py
+++ analytics/event_bridge.py
@@ -4,20 +4,25 @@ from kernel.events import EventStream
 class EventBridge:
 
     def __init__(self):
+
         self.stream = EventStream()
 
 
-    def collect(self, events):
+    def collect(
+        self,
+        events
+    ):
 
         for event in events:
+
             self.stream.add(event)
 
 
-    def get_events(self):
+    def get_events(
+        self
+    ):
 
         return self.stream.get_all()
-
-class EventBridge:
 
 
     def process_events(
@@ -25,9 +30,4 @@ class EventBridge:
         session
     ):
 
-        events = (
-            session.event_stream.events
-        )
-
-
-        return events
+        return session.event_stream.events
