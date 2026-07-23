cf2741b2c32cb105dec0238724c4ebea20c3f844
HammamNM <hammamomer304@gmail.com>
2026-07-23 14:25:09 +0200

    Update orchestrator.py

--- simulation/orchestrator.py
+++ simulation/orchestrator.py
@@ -6,21 +6,30 @@ from simulation.actions import (
 )
 
 from simulation.investigation_engine import InvestigationEngine
+from simulation.timeline import TimelineEngine
+from simulation.rules import RuleEngine
+
 from backend.case_loader import CaseLoader
-from outcome_engine.mapper import OutcomeMapper
+
 from decision_engine.engine import DecisionEngine
 from decision_engine.recorder import DecisionRecorder
 
 from outcome_engine.engine import OutcomeEngine
-from simulation.timeline import TimelineEngine
-from simulation.rules import RuleEngine
+from outcome_engine.mapper import OutcomeMapper
 
 
 class SimulationOrchestrator:
 
-    def __init__(self, session):
+    def __init__(
+        self,
+        session
+    ):
 
         self.session = session
+
+        self.case_loader = CaseLoader()
+
+        self.investigation_engine = InvestigationEngine()
 
         self.rule_engine = RuleEngine()
 
@@ -35,72 +44,7 @@ class SimulationOrchestrator:
         self.outcome_engine = OutcomeEngine()
 
         self.outcome_mapper = OutcomeMapper()
-     
-        self.case_loader = CaseLoader()
 
-        self.investigation_engine = InvestigationEngine()
-    
-    def process_student_action(self, action):
-
-        outcome_event = (
-            self.outcome_mapper
-            .map_action(action)
-        )
-        self.dispatch_action(
-            action
-        )
-        if outcome_event:
-
-            self.outcome_engine.process_event(
-                outcome_event.event_type,
-                self.session.outcome
-            )
-
-
-        self.rule_engine.process_action(
-            action,
-            self.session.state
-        )
-
-
-        decision_profile = (
-            self.decision_recorder.record(
-                self.session,
-                action
-            )
-        )
-
-
-        self.session.event_stream.add(
-            {
-                "event_type": "DECISION_PROFILE",
-                "content": decision_profile
-            }
-        )
-
-
-        for event in self.rule_engine.events:
-
-            self.session.event_stream.add(event)
-
-
-        self.rule_engine.events.clear()
-
-
-        self.session.clock.advance(1)
-
-
-        timeline_events = (
-            self.timeline_engine
-            .get_events_at_time(
-                self.session.clock.get_time()
-            )
-        )
-
-
-        for event in timeline_events:
-
-            self.session.event_stream.add(event)
 
     def load_case(
         self,
@@ -113,55 +57,112 @@ class SimulationOrchestrator:
             )
         )
 
+
     def dispatch_action(
-    self,
-    action
-):
+        self,
+        action
+    ):
 
-    if action in INVESTIGATION_ACTIONS:
+        if action in INVESTIGATION_ACTIONS:
 
-        self.investigation_engine.process_action(
-            action,
-            self.session.active_case,
-            self.session.event_stream
-        )
+            self.investigation_engine.process_action(
+                action,
+                self.session.active_case,
+                self.session.event_stream
+            )
+
+        elif action in THERAPEUTIC_ACTIONS:
+
+            self.rule_engine.process_action(
+                action,
+                self.session.state
+            )
+
+        elif action in DIAGNOSTIC_ACTIONS:
+
+            self.rule_engine.process_action(
+                action,
+                self.session.state
+            )
+
+        elif action in HISTORY_ACTIONS:
+
+            self.session.event_stream.add(
+                {
+                    "event_type": "HISTORY_RESPONSE_REQUEST",
+                    "content": action
+                }
+            )
 
 
-    elif action in THERAPEUTIC_ACTIONS:
+    def process_student_action(
+        self,
+        action
+    ):
 
-        self.rule_engine.process_action(
-            action,
-            self.session.state
-        )
-
-
-    elif action in DIAGNOSTIC_ACTIONS:
-
-        self.rule_engine.process_action(
-            action,
-            self.session.state
-        )
-
-
-    elif action in HISTORY_ACTIONS:
-
-        self.session.event_stream.add(
-            {
-                "event_type": "HISTORY_RESPONSE_REQUEST",
-                "content": action
-            }
-        )
-
-    def run_session(
-    self,
-    actions
-):
-
-    for action in actions:
-
-        self.process_student_action(
+        self.dispatch_action(
             action
         )
 
+        outcome_event = (
+            self.outcome_mapper.map_action(
+                action
+            )
+        )
 
-    return self.session
+        if outcome_event:
+
+            self.outcome_engine.process_event(
+                outcome_event.event_type,
+                self.session.outcome
+            )
+
+        decision_profile = (
+            self.decision_recorder.record(
+                self.session,
+                action
+            )
+        )
+
+        self.session.event_stream.add(
+            {
+                "event_type": "DECISION_PROFILE",
+                "content": decision_profile
+            }
+        )
+
+        for event in self.rule_engine.events:
+
+            self.session.event_stream.add(
+                event
+            )
+
+        self.rule_engine.events.clear()
+
+        self.session.clock.advance(1)
+
+        timeline_events = (
+            self.timeline_engine.get_events_at_time(
+                self.session.clock.get_time()
+            )
+        )
+
+        for event in timeline_events:
+
+            self.session.event_stream.add(
+                event
+            )
+
+
+    def run_session(
+        self,
+        actions
+    ):
+
+        for action in actions:
+
+            self.process_student_action(
+                action
+            )
+
+        return self.session
