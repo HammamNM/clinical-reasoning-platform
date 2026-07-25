b79c8bce8bfb0587ab70076be2f85b71bce07caa
HammamNM <hammamomer304@gmail.com>
2026-07-25 09:16:43 +0200

    Create reasoning_graph_builder.py

---
+++ kernel/reasoning_graph_builder.py
@@ -0,0 +1,138 @@
+from kernel.models import (
+    ReasoningUnit,
+    PrimitiveType
+)
+
+from kernel.graph import ReasoningGraph
+
+
+
+class ReasoningGraphBuilder:
+
+
+    def __init__(
+        self
+    ):
+
+        self.graph = ReasoningGraph()
+
+        self.counter = 0
+
+
+
+    def create_node_id(
+        self
+    ):
+
+        self.counter += 1
+
+        return str(
+            self.counter
+        )
+
+
+
+    def process_event(
+        self,
+        event
+    ):
+
+        primitive = (
+            self.classify_event(
+                event.event_type
+            )
+        )
+
+
+        if primitive is None:
+
+            return None
+
+
+        node = ReasoningUnit(
+
+            id=self.create_node_id(),
+
+            primitive=primitive,
+
+            content=str(
+                event.payload
+            )
+
+        )
+
+
+        self.graph.add_node(
+            node
+        )
+
+
+        self.connect_previous(
+            node.id
+        )
+
+
+        return node
+
+
+
+    def classify_event(
+        self,
+        event_type
+    ):
+
+
+        mapping = {
+
+
+            "HISTORY_RESPONSE_REQUEST":
+
+                PrimitiveType.OBSERVATION,
+
+
+            "INVESTIGATION_RESULT":
+
+                PrimitiveType.INVESTIGATION,
+
+
+            "DECISION_ASSESSMENT":
+
+                PrimitiveType.DECISION,
+
+
+            "OUTCOME_UPDATED":
+
+                PrimitiveType.OUTCOME
+
+        }
+
+
+        return mapping.get(
+            event_type
+        )
+
+
+
+    def connect_previous(
+        self,
+        current_id
+    ):
+
+        nodes = self.graph.nodes
+
+
+        if len(nodes) < 2:
+
+            return
+
+
+        previous = nodes[-2]
+
+
+        self.graph.add_edge(
+
+            previous.id,
+
+            current_id
+
+        )
