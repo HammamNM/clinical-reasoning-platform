d1bf02b731a62955f0055f970fa76e2a513c268c
HammamNM <hammamomer304@gmail.com>
2026-08-08 08:42:43 +0200

    Update engine.py

--- simulation/engine.py
+++ simulation/engine.py
@@ -1,1 +1,45 @@
+from simulation.orchestrator import (
+    SimulationOrchestrator
+)
 
+
+
+class SimulationEngine:
+
+
+    def __init__(
+        self,
+        runtime
+    ):
+
+        self.runtime = runtime
+
+        self.orchestrator = (
+
+            SimulationOrchestrator(
+                runtime
+            )
+
+        )
+
+
+
+    def execute_action(
+        self,
+        action
+    ):
+
+        self.orchestrator.process_student_action(
+            action
+        )
+
+
+
+    def run(
+        self,
+        actions
+    ):
+
+        return self.orchestrator.run_session(
+            actions
+        )
