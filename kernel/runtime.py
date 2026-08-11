3ca71d10e88134aabd37ac396cea8725ba27c6a6
HammamNM <hammamomer304@gmail.com>
2026-08-11 15:43:48 +0200

    Update runtime.py

--- kernel/runtime.py
+++ kernel/runtime.py
@@ -18,6 +18,10 @@ from kernel.reasoning_state_updater import (
     ReasoningStateUpdater
 )
 
+from kernel.reasoning_report_builder import (
+    ReasoningReportBuilder
+)
+
 
 class KernelRuntime:
 
@@ -28,87 +32,38 @@ class KernelRuntime:
     ):
 
         self.session = (
-
             session
-
             if session
-
             else KernelSession()
-
         )
 
-
-        self.registry = (
-            EngineRegistry()
-        )
-
+        self.registry = EngineRegistry()
 
         self.event_queue = []
-
 
         self.graph_builder = (
             ReasoningGraphBuilder()
         )
 
-
         self.reasoning_updater = (
             ReasoningStateUpdater()
         )
 
+        self.report_builder = (
+            ReasoningReportBuilder()
+        )
 
-        self.scenario_engine = None
+        self.last_report = None
 
 
     def register_engine(
         self,
-        engine,
-        priority=100
+        engine
     ):
 
         self.registry.register(
-
-            engine,
-
-            priority
-
+            engine
         )
-
-
-    def set_scenario_engine(
-        self,
-        scenario_engine
-    ):
-
-        self.scenario_engine = (
-            scenario_engine
-        )
-
-
-    def initialize(
-        self
-    ):
-
-        if self.scenario_engine is None:
-
-            return
-
-
-        initialization_event = (
-
-            self.scenario_engine.initialize(
-
-                self.session
-
-            )
-
-        )
-
-
-        if initialization_event is not None:
-
-            self.publish(
-                initialization_event
-            )
 
 
     def publish(
@@ -122,11 +77,8 @@ class KernelRuntime:
         ):
 
             raise TypeError(
-
                 "KernelRuntime accepts only ReasoningEvent"
-
             )
-
 
         self.event_queue.append(
             event
@@ -139,13 +91,20 @@ class KernelRuntime:
 
         while self.event_queue:
 
-            event = (
-                self.event_queue.pop(0)
-            )
+            event = self.event_queue.pop(0)
 
 
             self.session.event_stream.publish(
                 event
+            )
+
+
+            self.reasoning_updater.update(
+
+                self.session.reasoning_state,
+
+                event
+
             )
 
 
@@ -155,7 +114,6 @@ class KernelRuntime:
 
 
             generated_events = (
-
                 self.registry.dispatch(
 
                     event,
@@ -163,14 +121,29 @@ class KernelRuntime:
                     self.session
 
                 )
-
             )
 
 
-            for generated_event in (
-                generated_events
-            ):
+            for generated_event in generated_events:
 
                 self.publish(
                     generated_event
                 )
+
+
+    def build_report(
+        self
+    ):
+
+        self.last_report = (
+            self.report_builder.build(
+
+                self.session,
+
+                self.graph_builder.graph
+
+            )
+        )
+
+
+        return self.last_report
